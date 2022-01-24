2ff9661c522a334548957ec45b0391cd055cb421
bara1999 <171068@ppu.edu.ps>
2022-01-24 16:35:18 +0200

    form-api

---
+++ app.js
@@ -0,0 +1,13 @@
+let express = require("express");
+let app = express();
+let bodyParser = require("body-parser");
+var cors = require('cors')
+let formsRouter = require("./controllers/formsController");
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use("/forms", formsRouter);
+app.listen(3000, () => {
+  console.log("app is running on port 3000");
+});
