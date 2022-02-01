dcfcc08730057a932db2f4b7c960eafab4cfe4e4
yamen <191036@gmail.com>
2022-02-01 15:11:46 +0300

    removed .env

--- currency-exchange-api/index.js
+++ currency-exchange-api/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-const config = require("dotenv").config();
-const port = process.env.PORT;
+// const config = require("dotenv").config();
+// const port = process.env.PORT;
+const port = 3001;
 var app = express();
-// app.use(cors());
+app.use(cors());
 app.get('*', (req, res) => {
-    res.status(404).send('404', { url: req.originalUrl });
+    res.status(404).send({ url: req.originalUrl });
 })
 app.listen(port, () => console.log(`listening on port ${port}`));
