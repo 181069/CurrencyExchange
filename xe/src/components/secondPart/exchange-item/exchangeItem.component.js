b76067f1f9c97428c2c86ee046ca53dedc732c67
MohammedIHajjaj <mohammedih31@gmail.com>
2022-02-04 21:56:36 +0200

    simple edit

---
+++ xe/src/components/secondPart/exchange-item/exchangeItem.component.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./exchange-item.module.css";
+
+const ExchangeItem = (props) => {
+  return (
+   <div>
+
+   </div>
+  );
+};
+
+export default ExchangeItem;
--- xe/src/components/secondPart/live-exchange/liveExchange.component.js
+++ xe/src/components/secondPart/live-exchange/liveExchange.component.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Switch, Button } from "antd";
+import { Switch, Button, Collapse } from "antd";
 import "antd/dist/antd.css";
 import classes from "./live-exchange.module.css";
 import Counter from "../counter/counter.component";
@@ -12,9 +12,8 @@ const LiveExchange = () => {
   const [inverse, setInverse] = useState(false);
   const [editClicked, setEditClicked] = useState(false);
   const [updateDate, setUpdateDate] = useState(new Date().toUTCString());
-  const [temp, setTemp] = useState([]);
-  const [finalTemp, setFinalTemp] = useState([]);
   const [defaultData, setDefaultData] = useState(["USD", "CAD", "GBP", "EUR"]);
+  const { Panel } = Collapse;
 
   const getCurrencyData = (c) => {
     fetch(`http://www.floatrates.com/daily/${c[0]}.json`)
@@ -24,6 +23,10 @@ const LiveExchange = () => {
         setData(fResult);
       })
       .catch((error) => console.log(error));
+  };
+
+  const handleDelete = (key) => {
+    setDefaultData(defaultData.filter((item) => item.key !== key));
   };
 
   useEffect(() => {
@@ -36,20 +39,21 @@ const LiveExchange = () => {
 
   return (
     <div className={classes.LiveExchange}>
-      <p>
-        {temp.map((val, key) => (
-          <div key={key}>{console.log(val.currencies)}</div>
-        ))}
-      </p>
       <h2 className={classes.title}>Live Exchange Rates</h2>
       <div className={classes.liveTable}>
         <div className={classes.tableHeader}>
           <div className={classes.tableHeaderElem}>
             Inverse{" "}
-            <Switch
-              className={classes.inverseBtn}
-              onChange={() => setInverse(!inverse)}
-            />
+            {editClicked ? (
+              <Switch disabled />
+            ) : (
+              <Switch
+                className={
+                  editClicked ? classes.disableBtn : classes.inverseBtn
+                }
+                onChange={() => setInverse(!inverse)}
+              />
+            )}
           </div>
           <div className={classes.tableHeaderElem}>Amount</div>
           <div className={classes.tableHeaderElem}>Change(24h)</div>
@@ -80,68 +84,108 @@ const LiveExchange = () => {
               {inverse ? <div>Inverse</div> : <div>1</div>}
             </div>
           </div>
-          {Object.keys(data).map((val, key) => (
-            <div key={key} className={classes.currencyRow}>
-              <div>
-                <button
-                  className={classes.currencyBtn}
-                  onClick={() => {
-                    setDefaultData([data[val].code, ...defaultData]);
-                  }}
-                >
-                  <img
-                    src={`https://flagcdn.com/32x24/${data[val].code
-                      .substring(0, 2)
-                      .toLowerCase()}.png`}
-                    alt={data[val].code}
-                  />
-                  <span className={classes.currencyBtnTxt}>
-                    {" "}
-                    {data[val].code}{" "}
-                  </span>
-                </button>
-              </div>
-              {inverse ? (
-                <div>{data[val].inverseRate.toFixed(4)}</div>
-              ) : (
-                <div>{data[val].rate.toFixed(4)}</div>
-              )}
-              <div>
-                {(
-                  (data[val].rate - data[val].inverseRate) /
-                  data[val].rate
-                ).toFixed(3)}
-                %
-              </div>
-              <div>
-                <Chart />
-              </div>
-              <div>
-                <div className={classes.btnDiv}>
-                  <button className={classes.sendBtn}>
-                    <FontAwesomeIcon
-                      icon={faPaperPlane}
-                      className={classes.sendIcon}
+          {Object.keys(data)
+            .filter((item1) =>
+              defaultData.some((item2) => item1 === item2.toLowerCase())
+            )
+            .map((val, key) => (
+              // I will move it to exchange-item component
+              <div key={key} className={classes.currencyRow}>
+                <div>
+                  <button
+                    className={classes.currencyBtn}
+                    onClick={() => {
+                      setDefaultData([data[val].code, ...defaultData]);
+                    }}
+                  >
+                    <img
+                      src={`https://flagcdn.com/32x24/${data[val].code
+                        .substring(0, 2)
+                        .toLowerCase()}.png`}
+                      alt={data[val].code}
                     />
-                    Send
+                    <span className={classes.currencyBtnTxt}>
+                      {" "}
+                      {data[val].code}{" "}
+                    </span>
                   </button>
-                  {editClicked && (
-                    <button className={classes.deleteBtn}>-</button>
-                  )}
+                </div>
+                {inverse ? (
+                  <div>{data[val].inverseRate.toFixed(4)}</div>
+                ) : (
+                  <div>{data[val].rate.toFixed(4)}</div>
+                )}
+                <div>
+                  {(
+                    (data[val].rate - data[val].inverseRate) /
+                    data[val].rate
+                  ).toFixed(3)}
+                  %
+                </div>
+                <div>
+                  <Chart />
+                </div>
+                <div>
+                  <div className={classes.btnDiv}>
+                    {editClicked ? (
+                      <button disabled className={classes.sendBtn}>
+                        <FontAwesomeIcon
+                          icon={faPaperPlane}
+                          className={classes.sendIcon}
+                        />
+                        Send
+                      </button>
+                    ) : (
+                      <button className={classes.sendBtn}>
+                        <FontAwesomeIcon
+                          icon={faPaperPlane}
+                          className={classes.sendIcon}
+                        />
+                        Send
+                      </button>
+                    )}
+                    {editClicked && (
+                      <button
+                        className={classes.deleteBtn}
+                        onClick={(e) => console.log(e)}
+                      >
+                        -
+                      </button>
+                    )}
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
         </div>
+        {/* Currency Div which contain add currency btn and the counter */}
         <div className={classes.currencyDiv}>
-          <div>
-            <button className={classes.addBtn}>
-              <span className={classes.addCircle}>
-                <span className={classes.addIcon}>+</span>
-              </span>
-              Add Currency
-            </button>
-          </div>
+          <Collapse defaultActiveKey={["1"]}>
+            <Panel
+              header={
+                <div>
+                  {editClicked ? (
+                    <button disabled className={classes.addBtn}>
+                      <span className={classes.addCircle}>
+                        <span className={classes.addIcon}>+</span>
+                      </span>
+                      Add Currency
+                    </button>
+                  ) : (
+                    <button className={classes.addBtn}>
+                      <span className={classes.addCircle}>
+                        <span className={classes.addIcon}>+</span>
+                      </span>
+                      Add Currency
+                    </button>
+                  )}
+                </div>
+              }
+              showArrow={false}
+            >
+              <p>sdgfdsfg</p>
+            </Panel>
+          </Collapse>
+
           <div className={classes.counterDiv}>
             <Counter />
             <p className={classes.counterP}>Last updated {updateDate}</p>
