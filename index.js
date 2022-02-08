5b0f43001a2f7ff2c9988c5ac116808f81b250b7
amalomarnaser <amalnaserelden@gmail.com>
2022-02-09 00:49:11 +0200

    second commit fatima hasanat

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var express = require("express");
 const bodyParser = require("body-parser");
 const recordController = require("./app/controllers/record");
-
+const alertController = require("./app/controllers/alert")
 const app = express();
 app.use(bodyParser.json());
 
 app.use("/record", recordController);
-
+app.use("/alert",  alertController);
 app.listen(3002, () => {
   console.log("the app is running ");
 });
