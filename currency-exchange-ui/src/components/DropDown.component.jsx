940a62578895f5e05f57270e75317d3f9d323fce
yamen <62008653+weaponized-fetus@users.noreply.github.com>
2022-02-09 15:45:37 +0200

    added dropdown component

---
+++ currency-exchange-ui/src/components/DropDown.component.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styles from "./DropDown.module.css";
+/**
+ *
+ * @param {{items:{title:string, desc:string, icon:string}[]}} props
+ * @returns
+ */
+const DropDown = (props) => {
+  return (
+    <div id={styles.cssmenu}>
+      {/* {props.title.map((title, index) => (
+        <li></li>
+      ))} */}
+      <a className={styles.dropdownBtn}>
+        <span>About Us</span>
+      </a>
+      <ul>
+        {props.items.map((item, index) => (
+          <li className={styles.dropDownItem}>
+            <a>
+              <span>{item.title}</span>
+              <br />
+              <span className={styles.itemDesc}>{item.desc}</span>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default DropDown;
