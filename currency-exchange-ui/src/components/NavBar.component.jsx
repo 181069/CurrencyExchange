f8b077e2c0ae8177d6a233331d806053cf795d6d
yamen <62008653+weaponized-fetus@users.noreply.github.com>
2022-02-09 15:46:20 +0200

    added navBar component

--- currency-exchange-ui/src/components/NavBar.component.jsx
+++ currency-exchange-ui/src/components/NavBar.component.jsx
@@ -1,19 +1,42 @@
 import React from "react";
+import DropDown from "./DropDown.component";
 
-import styles from './NavBar.module.css';
+import styles from "./NavBar.module.css";
+
 const NavBar = (props) => {
+  /**
+   *
+   * @param { string } title
+   * @param { string } desc
+   * @param { string } icon
+   * @returns
+   */
+  const createMenuItem = (title, desc, icon) => {
+    return { title: title, desc: desc, icon: icon };
+  };
+
+  const sendMoneyMenuItems = [
+    createMenuItem(
+      "Send Money",
+      "Get a quote for fast and secure money transfers",
+      ""
+    ),
+    createMenuItem("Sign Up", "", ""),
+    createMenuItem("Compare rates", "", ""),
+  ];
+
   return (
     <div className={styles.NavBar}>
-      <div className="home"></div>
+      <div className="home">XE</div>
       <div className={styles.ribbon}>
-        <div>Converter</div>
-        <div>Send money</div>
-        <div>Business and API</div>
-        <div>Tools</div>
-        <div>Resources</div>
+        <DropDown title="Converter" items={sendMoneyMenuItems} />
+        <DropDown title="Send money" items={sendMoneyMenuItems} />
+        <DropDown title="Business and API" items={sendMoneyMenuItems} />
+        <DropDown title="Tools" items={sendMoneyMenuItems} />
+        <DropDown title="Resources" items={sendMoneyMenuItems} />
       </div>
       <div className="trailer">
-          <button>login</button>
+        <button>login</button>
       </div>
     </div>
   );
