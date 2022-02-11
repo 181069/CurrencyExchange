7c168a0e5b911ab9d956fd59806e965bc3a804a8
yamen <62008653+weaponized-fetus@users.noreply.github.com>
2022-02-11 21:26:10 +0200

    added routing

--- currency-exchange-ui/src/App.js
+++ currency-exchange-ui/src/App.js
@@ -1,12 +1,30 @@
 import "./App.css";
 import LiveExchangeRates from "./components/LiveExchangeRates.component";
 import NavBar from "./components/NavBar.component";
+import TabBox from "./components/TabBox.component";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CurrencyCharts from "./routes/currencycharts";
+import RateAlerts from "./routes/ratealerts";
+import CurrencyConvert from "./routes/currencyconvert";
+import SendMoney from "./routes/send-money";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <LiveExchangeRates currency={["dummy payload"]}/>
+      <BrowserRouter>
+        <TabBox />
+
+        <Routes>
+          <Route path="/" element={<CurrencyConvert />} />
+          <Route path="/currencycharts" element={<CurrencyCharts />} />
+          <Route path="/ratealerts" element={<RateAlerts />} />
+          <Route path="/currencyconverter" element={<CurrencyConvert />} />
+          <Route path="/send-money" element={<SendMoney />} />
+        </Routes>
+        <LiveExchangeRates currency={["dummy payload"]} />
+      </BrowserRouter>
     </div>
   );
 }
