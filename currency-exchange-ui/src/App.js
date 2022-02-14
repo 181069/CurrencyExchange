db84185d8fe6b081acc45da38991105f6261b3c9
yamen <62008653+weaponized-fetus@users.noreply.github.com>
2022-02-14 22:24:25 +0200

    rearranged routes+ added heading + tabBox wrapper

--- currency-exchange-ui/src/App.js
+++ currency-exchange-ui/src/App.js
@@ -8,21 +8,30 @@ import CurrencyCharts from "./routes/currencycharts";
 import RateAlerts from "./routes/ratealerts";
 import CurrencyConvert from "./routes/currencyconvert";
 import SendMoney from "./routes/send-money";
+import RangeChart from "./components/RangeChart.component";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <div className="main-heading">
+        <h1>The World's Trusted Currency Authority</h1>
+        <h4>
+          Check exchange rates, send money internationally, and free currency
+          tools
+        </h4>
+      </div>
       <BrowserRouter>
         <TabBox />
-
-        <Routes>
-          <Route path="/" element={<CurrencyConvert />} />
-          <Route path="/currencycharts" element={<CurrencyCharts />} />
-          <Route path="/ratealerts" element={<RateAlerts />} />
-          <Route path="/currencyconverter" element={<CurrencyConvert />} />
-          <Route path="/send-money" element={<SendMoney />} />
-        </Routes>
+        <div className="tabBox-wrapper">
+          <Routes>
+            <Route path="/" element={<CurrencyConvert />} />
+            <Route path="/currencyconverter" element={<CurrencyConvert />} />
+            <Route path="/currencycharts" element={<CurrencyCharts />} />
+            <Route path="/send-money" element={<SendMoney />} />
+            <Route path="/ratealerts" element={<RateAlerts />} />
+          </Routes>
+        </div>
         <LiveExchangeRates currency={["dummy payload"]} />
       </BrowserRouter>
     </div>
